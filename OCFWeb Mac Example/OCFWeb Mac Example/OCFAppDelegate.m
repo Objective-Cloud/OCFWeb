aa5dc14badc6a4f943203592c73d8fe7ccdc3c54
Christian Kienle <me@christian-kienle.de>
2013-08-04 13:26:19 +0200

    nicer example

--- OCFWeb Mac Example/OCFWeb Mac Example/OCFAppDelegate.m
+++ OCFWeb Mac Example/OCFWeb Mac Example/OCFAppDelegate.m
@@ -1,8 +1,36 @@
 #import "OCFAppDelegate.h"
+#import <OCFWeb/OCFWeb.h>
+
+@interface OCFAppDelegate ()
+
+@property (nonatomic, strong) OCFWebApplication *app;
+@property (nonatomic, strong) NSMutableArray *persons; // contains NSDictionary instances
+
+@end
 
 @implementation OCFAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
+    self.persons = [@[ @{ @"firstName" : @"christian", @"lastName" : @"kienle" },
+                          @{ @"firstName" : @"amin", @"lastName" : @"negm-awad" },
+                          @{ @"firstName" : @"bill", @"lastName" : @"gates" } ] mutableCopy];
+
+    self.app = [OCFWebApplication new];
+    
+    self.app[@"GET"][@"/persons"]  = ^(OCFRequest *request, OCFResponseHandler respondWith) {
+        respondWith([OCFMustache newMustacheWithName:@"Persons" object:@{@"persons" : self.persons}]);
+    };
+    
+    self.app[@"POST"][@"/persons"]  = ^(OCFRequest *request, OCFResponseHandler respondWith) {
+        [self.persons addObject:request.parameters];
+        respondWith(@"Added!");
+    };
+    
+    [self.app run];
+    
+    NSString *address = [NSString stringWithFormat:@"http://127.0.0.1:%lu/persons", self.app.port];
+    NSURL *result = [NSURL URLWithString:address];
+    [[NSWorkspace sharedWorkspace] openURL:result];
 }
 
 @end
