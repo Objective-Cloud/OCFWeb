415285f6779a88838e13add2321bea914b845ec6
Christian Kienle <me@christian-kienle.de>
2013-08-04 11:50:27 +0200

    cleans up the mac example

--- OCFWeb Mac Example/OCFWeb Mac Example/OCFAppDelegate.m
+++ OCFWeb Mac Example/OCFWeb Mac Example/OCFAppDelegate.m
@@ -1,18 +1,8 @@
-//
-//  OCFAppDelegate.m
-//  OCFWeb Mac Example
-//
-//  Created by cmk on 8/4/13.
-//  Copyright (c) 2013 Objective-Cloud.com. All rights reserved.
-//
-
 #import "OCFAppDelegate.h"
 
 @implementation OCFAppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-    // Insert code here to initialize your application
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
 }
 
 @end
